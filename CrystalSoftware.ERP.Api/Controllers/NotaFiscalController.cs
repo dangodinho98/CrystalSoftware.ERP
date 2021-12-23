30fa78c01222757d8339db84c3b305c1e615305e
Daniel Godinho Soares <dangodinho98@gmail.com>
2021-12-23 15:42:20 -0300

    Fix/adicionando jquery validation (#13)
    * Adicionando jquery validation
    
    Co-authored-by: Daniel Soares <Daniel.Soares@f1solutions.com.au>

---
+++ CrystalSoftware.ERP.Api/Controllers/NotaFiscalController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace CrystalSoftware.ERP.Api.Controllers
+{
+    [Authorize]
+    public class NotaFiscalController : Controller
+    {
+        public IActionResult ImportarXml()
+        {
+            return View();
+        }
+
+        [Authorize]
+        [HttpPost]
+        public async Task<IActionResult> ImportarXml(object request)
+        {
+            //var useCaseResponse = await _getAccountUseCase.Execute(request);
+            //if (useCaseResponse.Status == UseCaseResponseKind.BadRequest)
+            //{
+            //    if (useCaseResponse.Errors.Any())
+            //    {
+            //        foreach (var item in useCaseResponse.Errors)
+            //            ModelState.AddModelError("", item.Message);
+            //    }
+
+            //}
+
+            //return Json(useCaseResponse.Result.ToList());
+            return null;
+        }
+    }
+}
